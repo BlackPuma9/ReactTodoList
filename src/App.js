50069559e19ffd733eeca07e4e0dcbc37fbb0aaa
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-01-10 21:56:24 +0100

    i wanna beer!!!

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import React, { useState } from "react";
 
 function App() {
   const [value, setValue] = useState('')
+  const [tasks, setTasks] = useState(['buy', 'bread', 'apple'])
+
+  const eventEnterHandler = (event) => {
+    setValue(event.target.value)
+  }
+
+  const eventHadler = () => { 
+    setTasks([...tasks, value])
+  }
 
   return (
-    <div>
-      <h1>ToDoList</h1>
-      <input 
-        type="text" 
-        value={value}
-        placeholder="Enter your task"
-        onChange={event => setValue(event.target.value)}
-        />
-        <button>Submit</button>
-    </div>
-  );
+      <div>
+        <h1>ToDoList</h1>
+        <input 
+          type="text" 
+          value={value}
+          placeholder="Enter your task"
+          onChange={eventEnterHandler}
+          />
+          <button onClick={eventHadler}>Submit</button>
+        <h3>Task list</h3> 
+          <ol>
+            {tasks.map((task, index) => <li key={index}>{task}</li>)}
+          </ol>
+      </div>
+    );
 }
 
 export default App;
