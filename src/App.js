94c0d1d5394a4070611e52ed0c25c639bd64a71f
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-01-10 19:54:03 +0100

    Added imput and submit button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState('')
+
   return (
     <div>
-      
+      <h1>ToDoList</h1>
+      <input 
+        type="text" 
+        value={value}
+        placeholder="Enter your task"
+        onChange={event => setValue(event.target.value)}
+        />
+        <button>Submit</button>
     </div>
   );
 }
