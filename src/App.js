1029d5671d1d49c9d75daf7ee09631bbe762f200
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-01-11 22:12:14 +0100

    updated tasks to object

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { useState } from "react";
 
 function App() {
   const [value, setValue] = useState('')
-  const [tasks, setTasks] = useState(['buy', 'bread', 'apple'])
+  const [tasks, setTasks] = useState([
+    {
+      name: 'buy',
+      isComplited: false
+    },
+    {
+      name: 'bread',
+      isComplited: false
+    }
+  ])
 
   const eventEnterHandler = (event) => {
     setValue(event.target.value)
   }
 
   const eventHadler = () => { 
-    setTasks([...tasks, value])
+    if (value.length !== 0) {
+      setTasks([...tasks, { name: value, isComplited: false }])
+      setValue('')
+    }
   }
 
   return (
@@ -24,7 +36,7 @@ function App() {
           <button onClick={eventHadler}>Submit</button>
         <h3>Task list</h3> 
           <ol>
-            {tasks.map((task, index) => <li key={index}>{task}</li>)}
+            {tasks.map((task, index) => <li key={index}>{task.name}</li>)}
           </ol>
       </div>
     );
