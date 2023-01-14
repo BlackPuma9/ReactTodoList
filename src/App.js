dee18be53ae2387c90603c1239be52ac5b25a2e0
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-01-14 20:39:30 +0100

    Tags added, delete btn

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ function App() {
   const [tasks, setTasks] = useState([
     {
       name: 'buy',
-      isComplited: false
+      isComplited: false,
+      tags: ['#shopping']
     },
     {
       name: 'bread',
-      isComplited: false
+      isComplited: true,
+      tags: ['#hobby']
     }
   ])
+  // const [tags, setTags] = useState(['#shopping', '#hobby', '#work'])
+  const [checkedOne, setCheckedOne] = useState(false);
+  const [checkedTwo, setCheckedTwo] = useState(false);
+  const [checkedThree, setCheckedThree] = useState(false);
 
   const eventEnterHandler = (event) => {
     setValue(event.target.value)
@@ -19,9 +25,31 @@ function App() {
 
   const eventHadler = () => { 
     if (value.length !== 0) {
-      setTasks([...tasks, { name: value, isComplited: false }])
+      const tags = []
+      if (checkedOne) {
+        tags.push('#shopping')
+      }
+
+      if (checkedTwo) {
+        tags.push('#work')
+      }
+
+      if (checkedThree) {
+        tags.push('#hobby')
+      }
+
+      setTasks([...tasks, { name: value, isComplited: false, tags }])
       setValue('')
+      setCheckedOne(false)
+      setCheckedTwo(false)
+      setCheckedThree(false)
+      
     }
+  }
+
+  const handleToggle = (index) => {
+    tasks[index].isComplited = !tasks[index].isComplited
+    setTasks([...tasks])
   }
 
   return (
@@ -32,11 +60,45 @@ function App() {
           value={value}
           placeholder="Enter your task"
           onChange={eventEnterHandler}
-          />
+          /><br/>
+          <label>
+            <input type="checkbox" 
+            checked={checkedOne}
+            onChange={() => setCheckedOne(!checkedOne)}
+            />
+            #shopping
+          </label>
+          <label>
+            <input type="checkbox" 
+            checked={checkedTwo}
+            onChange={() => setCheckedTwo(!checkedTwo)}
+            />
+            #work
+          </label>
+          <label>
+            <input type="checkbox" 
+            checked={checkedThree}
+            onChange={() => setCheckedThree(!checkedThree)}
+            />
+            #hobby
+          </label><br/>
           <button onClick={eventHadler}>Submit</button>
         <h3>Task list</h3> 
           <ol>
-            {tasks.map((task, index) => <li key={index}>{task.name}</li>)}
+            {tasks.map((task, index) => {
+              const { name, isComplited, tags } = task
+              
+              return <li key={index}>
+                  {isComplited ? <del>{name}</del> : name } 
+                  <p>{tags.join(', ')}</p>
+                  <button
+                  onClick={() => handleToggle(index)}
+                  >{isComplited ? 'Uncomplete' : 'Complete'}</button>
+                  <button onClick={() => {
+                    setTasks([...tasks.filter((task, i) => i !== index)])
+                  }}>Remove</button>
+                </li>
+            })}
           </ol>
       </div>
     );
